8a701d27dd14223855b1901411c4f91bfe7b3c01
joomong0126 <lhbmdj@gmail.com>
2024-02-07 14:34:59 +0900

    지고,지네,지게 추가

--- code/03_preprocessing.py
+++ code/03_preprocessing.py
@@ -11,8 +11,7 @@ stopwords = stopwords + [ '찍다', '양도', '사장', '다녀오다',
 '잇다', '볶다', '모시', '오름', '포장', '웨이팅','넘다',
  '진짜', '절대', '먹다',  '니당다', '다시다','아깝다','번창','직원','찌다','매다',
 '집다','기고','이담','질도','기질','우리동네','동네','숨다','발견','이집','여기다',
- '나용','나다','추하다','자다','오다','가요','싶다','이예','일산','굳다']
-
+ '나용','나다','추하다','자다','오다','가요','싶다','이예','일산','굳다','지고', '지게', '지네']
 okt = Okt()
 cleaned_sentences = []
 for review in df.reviews:
@@ -32,12 +31,25 @@ for review in df.reviews:
     cleaned_sentences.append(cleaned_sentence)
 df['reviews'] = cleaned_sentences
 df.dropna(inplace=True)
-df.to_csv('./cleaned_naver_reviews.csv', index=False)
+df.to_csv('./cleaned_naver.csv', index=False)
 
 print(df.head())
 df.info()
 
-df = pd.read_csv('./cleaned_naver_reviews.csv')
+df = pd.read_csv('./cleaned_naver.csv')
 df.dropna(inplace=True)
 df.info()
-df.to_csv('./cleaned_naver_reviews.csv', index=False)
+df.to_csv('./cleaned_naver.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
